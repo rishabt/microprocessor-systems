fc53d66b6eeff3ad7a547d4a2eb691077dc05ce2
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2016-01-31 04:22:36 -0500

    Implement KalmanFilter in C and assembly

--- Lab1_STM32F4Cube_Base_project/Sources/Kalmanfilter_asm.s
+++ Lab1_STM32F4Cube_Base_project/Sources/Kalmanfilter_asm.s
@@ -1,6 +1,52 @@
 	AREA myCode, CODE, READONLY
 	EXPORT Kalmanfilter_asm
 Kalmanfilter_asm
+		; R0 --> Input array base address ;
+		; R1 --> Output array base address ;
+		; R2 --> Kalman state base address : q, r, x, p, k ;
+		; R3 --> Length of input array : int ;
+		; R4 --> i
+		; S0 --> State variable : x
 		
+		; S1 --> q
+		; S2 --> r
+		; S3 --> p
+		; S4 --> k
+		; S5 --> R0[i]
+		; S6 --> tmp
+		
+		; S9 --> 1.0
+		
+		VLDR S1, [R2]             		; load q
+		VLDR S2, [R2, #0X04]		  		; load r
+		VLDR S3, [R2, #0X0C]					; load p
+		VLDR S4, [R2, #0X10]					; load k
+		
+		VLDR S0, [R2, #0X08]					; load x_(i-1)
+		
+		VMOV.F32 S9, #1.0
+		LDR R4, =0x0									; i = 0
+		
+loop
+		ADD R5, R0, R4								; tmp = R0[i] address
+		VLDR S5, [R5]									; Get value of R0[i]
+		; Update Filter State ;
+		VADD.F32 S3, S3, S1						; p = p+q
+		VADD.F32 S6, S3, S2						; tmp = p+r
+		VDIV.F32 S4, S3, S6						; k = p/tmp
+		VSUB.F32 S6, S5, S0						; tmp = input[i] - x_(i-1)
+		VMUL.F32 S6, S6, S4						; tmp = tmp * k
+		VADD.F32 S0, S0, S6						; x = x_(i-1) + tmp
+		VSUB.F32 S6, S9, S4						; tmp = 1 - k
+		VMUL.F32 S3, S3, S6						; p = p * tmp
+		; Save value to output array ;
+		ADD R5, R1, R4								; tmp = R1[i] address
+		VSTR.F32 S0, [R5]									; Output[i] = S0
+		ADD R4, R4, #0x04							; increment i
+		
+		LSR R5, R4, #0x02							; divide i by 4 to normalize to index
+		SUBS R5, R5, R3								; subtract and update CPSR
+		BNE loop											; if i != length (in other words i<Length) loop
+		BX	LR
 	
 	END
--- Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -11,8 +11,50 @@ typedef struct kalman
 	float k; // kalman gain (gain applied to estimation error and added to previous state estimate to determine the present state estimate)
 } kalman_state;
 
+extern int Kalmanfilter_asm(float* InputArray, float* OutputArray, kalman_state* kstate, int Length);
+
+int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, int Length)
+{
+	int i;
+	for(i = 0; i < Length; i++)
+	{
+		kstate->p = kstate->p + kstate->q;
+		kstate->k = kstate->p/(kstate->p + kstate->r);
+		kstate->x = kstate->x + (kstate->k * (InputArray[i] - kstate->x));
+		kstate->p = (1 - kstate->k) * kstate->p;
+		
+		OutputArray[i] = kstate->x; 
+	}
+	
+	return 0;
+}
+
 int main()
 {
-
+	float input[5] = {10.0, 12.0, 13.0, 14.0, 15.0};
+	float output[5];
+	int length = 5;
+	
+	int i;
+	
+	kalman_state ks;
+	ks.q = 1;
+	ks.r = 2;
+	ks.x = 3;
+	ks.p = 4;
+	ks.k = 6;
+	
+	//float *a, *b;
+	//a = &balance[0];
+	//b = &balance[1];
+	//Kalmanfilter_asm(&input[0], &output[0], &ks, length);
+	
+	Kalmanfilter_C(&input[0], &output[0], &ks, length);
+	
+	for(i = 0; i < 5; i++)
+	{
+		printf("%f\n", output[i]);
+	}
+	
 	return 0;
 }
